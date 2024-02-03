f2e3a26369d3261dd8935e1e8422d55715a46aed
ksm1569 <tnals1564@naver.com>
2024-02-03 21:08:06 +0900

    접속 대기 웹페이지 개발

--- flow/src/main/java/com/smsoft/flow/controller/WaitingRoomController.java
+++ flow/src/main/java/com/smsoft/flow/controller/WaitingRoomController.java
@@ -1,19 +1,38 @@
 package com.smsoft.flow.controller;
 
+import com.smsoft.flow.service.UserQueueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
+@RequiredArgsConstructor
 @Controller
 public class WaitingRoomController {
 
+    private final UserQueueService userQueueService;
+
     @GetMapping("/waiting-room")
     Mono<Rendering> waitingRoomPage(@RequestParam(name = "queue", defaultValue = "default") String queue,
-                                    @RequestParam(name = "user_id") Long userId
+                                    @RequestParam(name = "user_id") Long userId,
+                                    @RequestParam(name = "redirect_url") String redirectUrl
     ) {
-        return Mono.just(Rendering.view("waiting-room.html")
-                .build());
+        // 1. 입장이 허용되는가
+        // 2. 이동
+        return userQueueService.isAllowed(queue, userId)
+                .filter(allowed -> allowed)
+                .flatMap(allowed -> Mono.just(Rendering.redirectTo(redirectUrl).build()))
+                .switchIfEmpty(
+                    userQueueService.registerWaitQueue(queue, userId)
+                            .onErrorResume(ex -> userQueueService.getRank(queue, userId))
+                            .map(rank -> Rendering.view("waiting-room.html")
+                                    .modelAttribute("number", rank)
+                                    .modelAttribute("userId", userId)
+                                    .modelAttribute("queue", queue)
+                                    .build()
+                            )
+                );
     }
 }
